be73c47604f4dd52d445629d340f82df1e49ba57
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-04 15:03:03 -0300

    Remoção da aplicação em mongoose

--- backend/src/controllers/SearchController.js
+++
@@ -1,9 +0,0 @@
-const Escala = require('../models/Escala');
-
-module.exports = {
-    async index(request, response) {
-        const {dia} = request.query;
-        const escalas = await Escala.find({dia});
-        return response.json({escalas});
-    },
-};
