1f6eb2ab457f6da6b2604da2299981d76ca67964
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-09 22:50:49 -0300

    @types

---
+++ mobile/src/interfaces/EscalaType.ts
@@ -0,0 +1,7 @@
+export default interface EscalaType {
+    id?: number;
+    data: string;
+    horario: string;
+    local: string;
+    ministro: number;
+}
---
+++ mobile/src/interfaces/MinistroType.ts
@@ -0,0 +1,5 @@
+export default interface MinistroType {
+    id: number;
+    name: string;
+    phone: string;
+}
